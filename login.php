b00223f07e9129689aaec6ef8f29c5dd535221e9
Aryaman47 <ohmshubham@gmail.com>
2023-01-28 19:13:46 +0530

    Edited obj=plan_id to obj=Service_id

--- login.php
+++ login.php
@@ -72,7 +72,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Login</title>
-	<style>
+<style>
 		html {
 			height: 100%;
 		}
@@ -139,13 +139,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			font-size: 12px;
 		}
 
-		.login-box form a {
+		.login-box form button {
 			position: relative;
+			background:none;
 			display: inline-block;
 			padding: 10px 20px;
 			color: #03e9f4;
 			font-size: 16px;
 			text-decoration: none;
+			border: none;
 			text-transform: uppercase;
 			overflow: hidden;
 			transition: .5s;
@@ -153,7 +155,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			letter-spacing: 4px
 		}
 
-		.login-box a:hover {
+		.login-box button:hover {
 			background: #03e9f4;
 			color: #fff;
 			border-radius: 5px;
@@ -163,12 +165,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 				0 0 100px #03e9f4;
 		}
 
-		.login-box a span {
+		.login-box button  span {
 			position: absolute;
 			display: block;
 		}
 
-		.login-box a span:nth-child(1) {
+		.login-box button  span:nth-child(1) {
 			top: 0;
 			left: -100%;
 			width: 100%;
@@ -188,7 +190,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			}
 		}
 
-		.login-box a span:nth-child(2) {
+		.login-box button  span:nth-child(2) {
 			top: -100%;
 			right: 0;
 			width: 2px;
@@ -209,7 +211,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			}
 		}
 
-		.login-box a span:nth-child(3) {
+		.login-box button  span:nth-child(3) {
 			bottom: 0;
 			right: -100%;
 			width: 100%;
@@ -230,7 +232,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			}
 		}
 
-		.login-box a span:nth-child(4) {
+		.login-box button  span:nth-child(4) {
 			bottom: -100%;
 			left: 0;
 			width: 2px;
